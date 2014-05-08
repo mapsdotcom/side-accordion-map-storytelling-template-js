bfcf6d3b8fec31140fb836d92ff65911a6b249b2
jessemaps <jesse@maps.com>
2014-05-08 00:26:01 -0500

    configure Levels of Freedom in the World storymap

--- app/series/Config.js
+++ app/series/Config.js
@@ -7,7 +7,7 @@ define([],
 			//The web map id
 			webmaps: [
 			{
-				id: "04744b7ad124458489ea821da3129352",
+			  id: "796c5f4a9a004ae0b12ad168bb14a3fa",
 				title: "",
 				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
 				showSingleTimeInstance: false,
@@ -16,7 +16,7 @@ define([],
 				// Set to true if you want the legend of this specific map to open when a user selects the tab for this map.
 				openLegendOnChange: false
 			},{
-				id: "d0f1c5ae11d24c778d597f8bbddcf85f",
+			  id: "0d83578f2aef4b8e9af218a6a0702c75",
 				title: "",
 				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
 				showSingleTimeInstance: false,
@@ -26,7 +26,7 @@ define([],
 				openLegendOnChange: false
 			},
 			{
-				id: "bb5a3da962ac4040b2f52f5869fddbd8",
+			  id: "06548aaf9c34460dbe4e35f3758f23b3",
 				title: "",
 				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
 				showSingleTimeInstance: false,
@@ -61,7 +61,7 @@ define([],
 			//	}
 			],
 			//Enter a title, if no title is specified, the first webmap's title is used.
-			title: "",
+			title: "Levels of Freedom in the World",
 			//Enter a subtitle, if no subtitle is specified, the first webmap's subtitle is used.
 			subtitle: "",
 			// If false, each tab will have a number on it. If true, the first tab will not have a number and the second tab will start counting at 1.
@@ -79,11 +79,11 @@ define([],
 			//Modify this to point to your sharing service URL if you are using the portal
 			sharingurl: "http://www.arcgis.com/sharing/rest/content/items",
 		  //Maximum zoom level allowed in the storymap (0 - 20)
-			maximumZoom: 20,
+			maximumZoom: 6,
 
       //Social Media Configurations
 		  //Include logo in header
-		  includeLogo: true,
+		  includeLogo: false,
 		  //Logo Image URL for header logo:
 		  logoImgUrl: "resources/images/app/MapscomLogo.png",
 		  //URL to load when logo is clicked:
